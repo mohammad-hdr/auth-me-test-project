e7ae06d6a1615f47870b76964fe742bf5e448338
Arman Heidari <armanhdr.js@gmail.com>
2025-07-18 18:55:27 +0330

    Add Iran Yekan font constants for multiple weights and styles

---
+++ src/constants/font.ts
@@ -0,0 +1,127 @@
+import localFont from "next/font/local";
+
+export const IranYekanXFont = localFont({
+    src: [
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-Heavy.woff2",
+            style: "normal",
+            weight: "1000",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-ExtraBlack.woff2",
+            style: "normal",
+            weight: "950",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-Black.woff2",
+            style: "normal",
+            weight: "900",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-ExtraBold.woff2",
+            style: "normal",
+            weight: "800",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-Bold.woff2",
+            style: "normal",
+            weight: "700",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-DemiBold.woff2",
+            style: "normal",
+            weight: "600",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-Medium.woff2",
+            style: "normal",
+            weight: "500",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-Regular.woff2",
+            style: "normal",
+            weight: "400",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-Light.woff2",
+            style: "normal",
+            weight: "300",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-UltraLight.woff2",
+            style: "normal",
+            weight: "200",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x/IRANYekanXFaNum-Thin.woff2",
+            style: "normal",
+            weight: "100",
+        },
+    ],
+    display: "swap",
+    style: "normal",
+    variable: "--iran-yekan-x",
+});
+
+export const IranYekanXEnNumFont = localFont({
+    src: [
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-Heavy.woff2",
+            style: "normal",
+            weight: "1000",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-ExtraBlack.woff2",
+            style: "normal",
+            weight: "950",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-Black.woff2",
+            style: "normal",
+            weight: "900",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-ExtraBold.woff2",
+            style: "normal",
+            weight: "800",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-Bold.woff2",
+            style: "normal",
+            weight: "700",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-DemiBold.woff2",
+            style: "normal",
+            weight: "600",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-Medium.woff2",
+            style: "normal",
+            weight: "500",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-Regular.woff2",
+            style: "normal",
+            weight: "400",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-Light.woff2",
+            style: "normal",
+            weight: "300",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-UltraLight.woff2",
+            style: "normal",
+            weight: "200",
+        },
+        {
+            path: "./../../public/fonts/iran-yekan-x-enNum/IRANYekanX-Thin.woff2",
+            style: "normal",
+            weight: "100",
+        },
+    ],
+    display: "swap",
+    style: "normal",
+    variable: "--iran-yekan-x-en-num",
+});
