568e661a16c5c6028793855d24c4e36a88c6481c
Arman Heidari <armanhdr.js@gmail.com>
2025-07-18 18:56:53 +0330

    Add authentication page and SCSS styles for login layout

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,9 @@
+import PhoneForm from "./_components/phone-form";
+
+export default function AuthPage() {
+    return (
+        <main className="login-page">
+            <PhoneForm />
+        </main>
+    );
+}
